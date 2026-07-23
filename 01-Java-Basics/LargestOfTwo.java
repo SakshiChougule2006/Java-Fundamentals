7973eeffeeb0f3bb153e540df0638ea33e71d78d
Sakshi Chougule <sakshichougule2728@gmail.com>
2026-07-23 18:55:52 +0530

    Add Largest of Two Numbers program

---
+++ 01-Java-Basics/LargestOfTwo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestOfTwo {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = in.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = in.nextInt();
+
+        if (a > b) 
+        {
+            System.out.println("Largest number is: " + a);
+        } 
+        else if (b > a) 
+        {
+            System.out.println("Largest number is: " + b);
+        } 
+        else
+        {
+            System.out.println("Both numbers are equal.");
+        }
+
+    }
+}
