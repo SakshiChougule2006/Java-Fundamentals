7d524de6f0c670eab1438d4229007d1f766601a1
Sakshi Chougule <sakshichougule2728@gmail.com>
2026-07-23 18:42:38 +0530

    Add Even Odd program

---
+++ 01-Java-Basics/EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenOdd
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter The Number");
+       int  a=in.nextInt();
+        if(a%2==0)
+        {
+            System.out.println("The Number is Even");
+        }
+        else
+        {
+            System.out.println("The Number is Odd");
+        }
+    }
+}
