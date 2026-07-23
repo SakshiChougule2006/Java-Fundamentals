02311273c532d565d2d9946f956dad1be8477963
Sakshi Chougule <sakshichougule2728@gmail.com>
2026-07-23 19:27:04 +0530

    Add Vowel or Consonant program

---
+++ 01-Java-Basics/VowelOrConsonant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant
+ {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = in.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+        {
+
+            System.out.println(ch + " is a Vowel.");
+        } 
+        else 
+        {
+            System.out.println(ch + " is a Consonant.");
+        }
+
+        
+    }
+}
