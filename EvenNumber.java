818da5e00195c5bafda84a4b86bee16783492871
Sakshi Chougule <sakshichougule2728@gmail.com>
2026-07-24 17:01:53 +0530

    Add Even Numbers program

---
+++ EvenNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class EvenNumber
+
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+
+        System.out.println("Enter the value of N:");
+
+        int n=in.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+            System.out.println(i);
+            }
+        }
+
+    }
+}
