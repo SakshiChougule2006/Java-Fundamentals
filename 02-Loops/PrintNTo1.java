42edac6d2104c84e2785fdce346f5ee985c035d6
Sakshi Chougule <sakshichougule2728@gmail.com>
2026-07-24 16:44:06 +0530

    Add Print N to 1 program

--- 02-Loops/PrintNTo1.java
+++ 02-Loops/PrintNTo1.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
-
-class Print1ToN
+public class PrintNTo1
 
 {
-
     public static void main(String args[])
-
     {
-
         Scanner in=new Scanner(System.in);
 
         System.out.println("Enter the value of N:");
 
         int n=in.nextInt();
 
-        for(int i=1;i<=n;i++)
-
+        for(int i=n;i>=1;i--)
         {
-
             System.out.println(i);
-
         }
 
     }
-
 }
